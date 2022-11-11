d33b893f49f5a7266c14a314b8ce74e1195aea64
Clark Scheff <clark@scheffsblend.com>
2022-11-11 09:56:50 -0800

    optimize and speedup checking for updates
    This diff simplifies the data being passed to
    just the name and appID(ref) as well as only
    makes one call to `flatpak remote-ls` to get
    all updatable flatpaks

--- src/FlatpakInfo.ts
+++ src/FlatpakInfo.ts
@@ -1,23 +1,13 @@
 export class FlatpakInfo {
     appID: string;
     name: string;
-    localHash: string;
-    remoteHash: string;
-    remote: string;
-    ref: string;
-    updateAvailable: boolean;
 
-    constructor(appID: string, name: string, localHash: string, remoteHash: string, remote: string, ref: string, updateAvailable: boolean) {
+    constructor(appID: string, name: string) {
         this.appID = appID;
         this.name = name;
-        this.localHash = localHash;
-        this.remoteHash = remoteHash;
-        this.remote = remote;
-        this.ref = ref;
-        this.updateAvailable = updateAvailable;
     }
 
     static fromJSON(json: any) {
-        return new FlatpakInfo(json.appID, json.name, json.localHash, json.remoteHash, json.remote, json.ref, json.updateAvailable);
+        return new FlatpakInfo(json.appID, json.name);
     }
 }
